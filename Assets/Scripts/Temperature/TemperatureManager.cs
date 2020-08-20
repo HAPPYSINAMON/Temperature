f8e105fc06887dcb0c8961cd6dbca6edde7cea72
검은경계 <toltolya@naver.com>
2020-08-21 00:52:51 +0900

    월드 온도 기초

---
+++ Assets/Scripts/Temperature/TemperatureManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemperatureManager : MonoBehaviour
+{
+    public int currentTemperature;
+
+    public const int TEMPERATURE_MIN = 0;
+    public const int TEMPERATURE_MAX = 50;
+
+    private void Awake()
+    {
+        currentTemperature = Random.Range(TEMPERATURE_MIN, TEMPERATURE_MAX + 1);
+    }
+}
