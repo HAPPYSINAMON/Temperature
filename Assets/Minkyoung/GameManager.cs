75f64dad228cf968f4c531566d74065698807d7c
HAPPYSINAMON <52099069+HAPPYSINAMON@users.noreply.github.com>
2020-08-21 02:37:13 +0900

    Update GameManager.cs

--- Assets/Minkyoung/GameManager.cs
+++ Assets/Minkyoung/GameManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance = null;
-
+    public Transform MyCores;
+    public Transform EnemyCores;
     public List<ControlCore> MyCore = new List<ControlCore> ();
     public List<ControlCore> EnemyCore = new List<ControlCore>();
 
@@ -38,6 +40,15 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        for(int i = 1; i<MyCores.childCount; i++)
+        {
+            MyCore.Add(MyCores.GetChild(i).GetComponent<ControlCore>());
+        }
+
+        for (int i = 0; i < EnemyCores.childCount; i++)
+        {
+            EnemyCore.Add(EnemyCores.GetChild(i).GetComponent<ControlCore>());
+        }
     }
 
     // Update is called once per frame
