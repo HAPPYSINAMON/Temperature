d316e556e9ae0e1ca6ca9998de9b6fd8120a5828
soso03185 <34054994+soso03185@users.noreply.github.com>
2020-08-21 01:23:50 +0900

    Add files via upload

---
+++ HP.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP : MonoBehaviour
+{
+    public int hpPoint = 1;
+    public int atkPoint = 1;
+    public int defPoint = 1;
+
+    void OnTirggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+            downHealth();
+        else if (collision.CompareTag("Portion"))
+            upHealth();
+    }
+
+    public void downHealth()
+    {
+        if (hpPoint > 1)
+        {
+            hpPoint--;
+            Debug.Log("downHealth");
+        }
+        else
+        {
+            Debug.Log("Die");
+        }
+    }
+
+    public void upHealth()
+    {
+        Debug.Log("up");
+    }
+
+}
