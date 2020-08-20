b97c493a11c238f39e2fa56f63f1390b0557d6e7
검은경계 <toltolya@naver.com>
2020-08-20 23:53:19 +0900

    캐릭터 Excel Importer

---
+++ Assets/Scripts/ExcelImporter/Example/Scripts/Entity/CharacterEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class CharacterEntity
+{
+	public int id;
+	public string name;
+	public int hp;
+	public int atk;
+}
