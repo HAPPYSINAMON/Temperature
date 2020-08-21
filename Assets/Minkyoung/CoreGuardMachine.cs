6b22cf92b2f770c7c7fbd861a9dcbca71f24a970
HAPPYSINAMON <52099069+HAPPYSINAMON@users.noreply.github.com>
2020-08-21 16:25:06 +0900

    가드 수정

---
+++ Assets/Minkyoung/CoreGuardMachine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreGuardMachine : GuardMachine
+{
+    public int num = 0;
+    public Transform ControlCoreTrans;
+
+    private float runningTime = 0;
+    private Vector2 newPos = new Vector2();
+
+    void Start()
+    {
+        runningTime = num * 400;
+        if (ControlCoreTrans == null)
+        {
+            ControlCoreTrans = GetComponentInParent<ControlCore>().gameObject.transform;
+        }
+        Enemy = ControlCoreTrans.GetComponent<Core>().Enemy;
+    }
+
+    // Use this for initialization
+    void Update()
+    {
+        runningTime += Time.deltaTime * speed;
+        if (!Die)
+        {
+            float x = radius * Mathf.Cos(runningTime) + ControlCoreTrans.position.x;
+            float y = radius * Mathf.Sin(runningTime) + ControlCoreTrans.position.y;
+            newPos = new Vector2(x, y);
+            transform.position = newPos;
+        }
+    }
+}
