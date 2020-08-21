acee3f89af166b5fcb09126d7653aa6b053c34c2
soso03185 <34054994+soso03185@users.noreply.github.com>
2020-08-21 20:34:13 +0900

    카메라 줌인,아웃

--- Assets/JeaHyun/CameraPlayer.cs
+++ Assets/JeaHyun/CameraPlayer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraPlayer : MonoBehaviour
 {
+    public float zoomSize = 5f;
 
     public GameObject target; // 카메라가 따라갈 대상
     public float moveSpeed; // 카메라 속도
@@ -11,11 +12,25 @@ public class CameraPlayer : MonoBehaviour
 
     void Update()
     {
- 
         if (target.gameObject != null)
         {
             targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
             this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
         }
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (zoomSize > 1)
+                zoomSize -= 0.3f;
+        }
+         if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if( zoomSize < 5)
+            {
+                zoomSize += 0.3f;
+            }
+        }
+        GetComponent<Camera>().orthographicSize = zoomSize;
+
     }
 }
