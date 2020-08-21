41cf47749528a725375191401f441b5e338f6e2e
HAPPYSINAMON <52099069+HAPPYSINAMON@users.noreply.github.com>
2020-08-21 13:06:41 +0900

    가드머신 추가

--- Assets/Minkyoung/ControlCore.cs
+++ Assets/Minkyoung/ControlCore.cs
@@ -6,9 +6,20 @@ public class ControlCore : Core
 {
     public int number = 0;
     public int Temp;
+    public List<GuardMachine> guardMachines = new List<GuardMachine>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            guardMachines.Add(transform.GetChild(i).GetComponent<GuardMachine>());
+            transform.GetChild(i).GetComponent<GuardMachine>().num = i;
+            transform.GetChild(i).GetComponent<GuardMachine>().ControlCoreTrans = transform;
+        }
+    }
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
--- Assets/Minkyoung/Core.cs
+++ Assets/Minkyoung/Core.cs
@@ -2,8 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Core : MonoBehaviour
+public class Core : MonoBehaviour, IDefender
 {
     public bool Enemy = false;
-    public int hp = 100;
+
+    public int HP { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public CharacterState characterState { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public void DamageProcess(int damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public int GetDef()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool IsDie()
+    {
+        throw new System.NotImplementedException();
+    }
 }
